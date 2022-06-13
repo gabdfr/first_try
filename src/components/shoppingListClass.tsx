e1b9c34960057bab74008d37298d23f3ffe97c3e
gabdfr <gabriel.dufour@viacesi.fr>
2022-06-13 23:01:59 +0200

    Adding main actor

--- src/components/shoppingListClass.tsx
+++ src/components/shoppingListClass.tsx
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 interface TitleProps {
     title: string;
     subtitle?: string;
+    mainActor?: string;
 }
-
+//this is a comment to try if the correct branch is setted
 class TitleClass extends Component<TitleProps> {
     render() {
-        const { title, subtitle } = this.props;
+        const { title, subtitle,mainActor } = this.props;
         return (
             <>
                 <h1>{title}</h1>
                 <h2>{subtitle}</h2>
+                <h3>{mainActor}</h3>
             </>
         );
     }
